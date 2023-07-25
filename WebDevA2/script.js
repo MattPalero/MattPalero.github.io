db2bfd03891d869e3446dbf554e6f6275ce51535
MattPalero <123877448+MattPalero@users.noreply.github.com>
2023-07-25 14:11:41 +0800

    A2 progress

--- WebDevA2/script.js
+++ WebDevA2/script.js
@@ -1,29 +1,26 @@
 
 //target all elements to save to constants
-const page1btn = document.querySelector("#page1btn");
-const page2btn = document.querySelector("#page2btn");
-const page3btn = document.querySelector("#page3btn");
+const page1btn = document.querySelector("#HomePg");
+const page2btn = document.querySelector("#RulesPg");
+const page3btn = document.querySelector("#PlaysPg");
+const page4btn = document.querySelector("#LegendsPg");
 var allpages = document.querySelectorAll(".page");
 //select all subtopic pages
 
 
-console.log(allpages);
-hideall();
+//console.log(allpages);
+
+// hideall();
+show(1); //show home page by default
+
 function hideall() { //function to hide all pages
     for (let onepage of allpages) { //go through all subtopic pages
         onepage.style.display = "none"; //hide it
     }
 }
 
-// page1btn.addEventListener("click",ShowPage1);
-
-// function ShowPage1(){
-//     let thisPage=document.querySelector("#page1");
-//     thisPage.style.display="block";
-// }
-
 function show(pgno) { //function to show selected page no
-    hideall();
+    hideall(); // hide all pages
     //select the page based on the parameter passed in
     let onepage = document.querySelector("#page" + pgno);
     //show the page
@@ -32,22 +29,17 @@ function show(pgno) { //function to show selected page no
 /*Listen for clicks on the buttons, assign anonymous
 eventhandler functions to call show function*/
 page1btn.addEventListener("click", function () {
-    show(1);
+    show(1); //show home page
 });
 page2btn.addEventListener("click", function () {
-    show(2);
+    show(2); //show history page
 });
 page3btn.addEventListener("click", function () {
-    show(3);
+    show(3); //show rules page
 });
-
-/*for hamMenu */
-const hamBtn=document.querySelector("#hamIcon");
-hamBtn.addEventListener("click",toggleMenus);
-const menuItemsList=document.querySelector("nav ul");
-function toggleMenus(){ /*open and close menu*/
-    menuItemsList.classList.toggle("menuShow");
-}//can optimize using toggle class with css transitions
+page4btn.addEventListener("click", function () {
+    show(4); //show legends page
+});
 
 
 
